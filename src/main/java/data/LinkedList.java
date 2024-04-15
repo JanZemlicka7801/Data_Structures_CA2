1b4e91b4b68c6e282425b764f1a6fcafcc4f70ce
JanZemlicka7801 <jan.zemlicka1@gmail.com>
2024-04-15 16:44:46 +0100

    Creted a LinkedList class which contains nodes each of nodes has data in for of an appointment which i created earlier.

---
+++ src/main/java/data/LinkedList.java
@@ -0,0 +1,52 @@
+package data;
+
+import objects.Appointment;
+
+public class LinkedList {
+
+    private Node head;
+    private Node tail;
+    private int size;
+
+    public LinkedList(){
+        head = null;
+        tail = null;
+        size = 0;
+    }
+
+    protected static class Node{
+        private Appointment data;
+        private Node next;
+        private Node previous;
+
+        public Node(Appointment data){
+            this.data = data;
+            next = null;
+            previous = null;
+        }
+
+        public Appointment getData() {
+            return data;
+        }
+
+        public void setData(Appointment data) {
+            this.data = data;
+        }
+
+        public Node getNext() {
+            return next;
+        }
+
+        public void setNext(Node next) {
+            this.next = next;
+        }
+
+        public Node getPrevious() {
+            return previous;
+        }
+
+        public void setPrevious(Node previous) {
+            this.previous = previous;
+        }
+    }
+}
