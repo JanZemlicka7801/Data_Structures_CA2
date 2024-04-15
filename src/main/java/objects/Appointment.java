df9aae6b5b2e8459b8e9976967009b4dac0bdb25
JanZemlicka7801 <jan.zemlicka1@gmail.com>
2024-04-15 15:54:59 +0100

    Created a properties of an appointment.

---
+++ src/main/java/objects/Appointment.java
@@ -0,0 +1,16 @@
+package objects;
+
+import java.time.*;
+import java.util.Objects;
+
+public class Appointment {
+    private String pFname;
+    private String pLname;
+    private LocalDate pDateOfBirth;
+    private String issue;
+    private LocalDate date;
+    private int triage;
+    private String docName;
+
+
+}
