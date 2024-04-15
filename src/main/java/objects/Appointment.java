333ee7971f0467f29d5f5676b87c8249ea9fdad0
JanZemlicka7801 <jan.zemlicka1@gmail.com>
2024-04-15 15:56:23 +0100

    Created a paramerised constructor for an Appointment.

--- src/main/java/objects/Appointment.java
+++ src/main/java/objects/Appointment.java
@@ -12,5 +12,25 @@ public class Appointment {
     private int triage;
     private String docName;
 
+    /**
+     * To create an appointment all information needs to be input.
+     *
+     * @param pFname objects.Patient's first name.
+     * @param pLname objects.Patient's last name.
+     * @param pDateOfBirth objects.Patient's date of birth.
+     * @param issue Reason why patient needs to come.
+     * @param date Date of an appointment.
+     * @param triage The triage level.
+     * @param docName Name of a doctor.
+     */
 
+    public Appointment(String pFname, String pLname, LocalDate pDateOfBirth, String issue, LocalDate date, int triage, String docName) {
+        this.pFname = pFname;
+        this.pLname = pLname;
+        this.pDateOfBirth = pDateOfBirth;
+        this.issue = issue;
+        this.date = date;
+        this.triage = triage;
+        this.docName = docName;
+    }
 }
