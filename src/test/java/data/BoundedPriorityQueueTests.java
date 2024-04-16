26ac39fe14e4581b8a503a691d2a4c139ab340d6
Dmytro Drahan <dmitrijrolkin@gmail.com>
2024-04-16 17:55:46 +0100

    Rearranged test folder structure. Added empty test class for Queue

---
+++ src/test/java/data/BoundedPriorityQueueTests.java
@@ -0,0 +1,36 @@
+package data;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BoundedPriorityQueueTests {
+
+    @Test
+    void isFull() {
+    }
+
+    @Test
+    void count() {
+    }
+
+    @Test
+    void poll() {
+    }
+
+    @Test
+    void peek() {
+    }
+
+    @Test
+    void offer() {
+    }
+
+    @Test
+    void remove() {
+    }
+
+    @Test
+    void add() {
+    }
+}
--- src/test/java/data/LinkedListTest.java
+++ src/test/java/data/LinkedListTest.java
@@ -1,5 +1,6 @@
+package data;
+
 import objects.Appointment;
-import data.LinkedList;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
