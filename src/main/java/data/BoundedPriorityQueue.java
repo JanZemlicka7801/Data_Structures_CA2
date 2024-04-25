06360e702b9b1c258a76364fc62d93fdd65819ec
JanZemlicka7801 <jan.zemlicka1@gmail.com>
2024-04-25 10:14:43 +0100

    isFull() checks if there is a space in queue.

--- src/main/java/data/BoundedPriorityQueue.java
+++ src/main/java/data/BoundedPriorityQueue.java
@@ -1,11 +1,19 @@
 package data;
 
-public class BoundedPriorityQueue {
+public class BoundedPriorityQueue extends LinkedList{
     private final int max;
     private final String doctorName;
 
     public BoundedPriorityQueue(int max, String doctorName) {
         this.max = max;
         this.doctorName = doctorName;
+    }
+
+    /**
+     * Checks if the queue is full. Checks if the queue has reached the maximum size of a set(maximum size is initialized in constructor).
+     * @return boolean indicating if a queue is full.
+     */
+    public boolean isFull(){
+        return super.size() == max;
     }
 }
